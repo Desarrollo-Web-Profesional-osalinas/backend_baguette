221f2be6c42fc3f573b4920f932812e7fc723ef5
omarsalinas3 <salinasomar996@gmail.com>
2026-03-19 13:50:36 -0600

    prueba vercel

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ dotenv.config()
 import { app } from './app.js'
 import { initBaseDeDatos } from './bd/init.js'
 
-/**
- * Iniciar el servidor Express después de conectar a la base de datos
- */
-try {
-    await initBaseDeDatos()
-    const PORT = process.env.PORT // ||3001
-    app.listen(PORT)
-    console.info(`Servidor Express ejecutandose sobre http://localhost:${PORT}`)
-} catch (err) {
-    console.error('Error connectandose a la Base de Datos:', err)
+await initBaseDeDatos()
+
+// Solo escucha en puerto si estamos en local
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.info(`Servidor en http://localhost:${PORT}`)
+  })
 }
+
+export default app
